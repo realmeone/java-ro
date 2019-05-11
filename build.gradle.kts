a875f9275259292db9f9196c7f0c5e27752b49ff
ashimida6 <ashimida6@github.com>
2019-05-12 01:26:57 +0800

    1. upgrade gradle and dependency jars

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,8 @@ plugins {
     java
     application
     idea
-    id("com.google.protobuf") version "0.8.6"
-    kotlin("jvm") version "1.3.11"
+    id("com.google.protobuf") version "0.8.8"
+    kotlin("jvm") version "1.3.31"
 }
 
 java {
@@ -21,9 +21,9 @@ java {
 }
 
 application {
-    group = "one.realme.krot"
+    group = "one.realme"
     version = "0.1.0"
-    mainClassName = "one.realme.krot.program.Main"
+    mainClassName = "one.realme.krot.program.Krot"
 }
 
 protobuf {
@@ -36,18 +36,18 @@ protobuf {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
-    implementation("io.netty:netty-all:4.1.32.Final")
-    implementation("com.google.guava:guava:27.0.1-jre")
+    implementation("io.netty:netty-all:4.1.36.Final")
+    implementation("com.google.guava:guava:27.1-jre")
     implementation("ch.qos.logback:logback-classic:1.2.3")
-    implementation("org.bouncycastle:bcprov-jdk15on:1.60")
-    implementation("org.rocksdb:rocksdbjni:5.15.10")
-    implementation("com.github.ajalt:clikt:1.6.0")
-    implementation("com.github.ajalt:mordant:1.2.0")
-    implementation("com.typesafe:config:1.3.3")
+    implementation("org.bouncycastle:bcprov-jdk15on:1.61")
+    implementation("org.rocksdb:rocksdbjni:6.0.1")
+    implementation("com.github.ajalt:clikt:1.7.0")
+    implementation("com.github.ajalt:mordant:1.2.1")
+    implementation("com.typesafe:config:1.3.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.1")
-    implementation("com.google.protobuf:protobuf-java:3.6.1")
+    implementation("com.google.protobuf:protobuf-java:3.7.1")
 
-    
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.3.1")
     testRuntime("org.junit.jupiter:junit-jupiter-engine:5.3.1")
@@ -63,13 +63,6 @@ tasks {
         testLogging {
             events("passed", "skipped", "failed")
         }
-    }
-
-    withType<Jar> {
-        manifest {
-            attributes["Main-Class"] = application.mainClassName
-        }
-        from(configurations.runtime.map { if (it.isDirectory) it else zipTree(it) })
     }
 
     withType<Tar> {
